6f6c55a3f469d82a1c040a48732a20ea7edf5e32
YoshinoriN <nayo-msoft@outlook.jp>
2017-04-16 03:55:50 +0900

    Initial commit

---
+++ RedmineUserImport/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+
+namespace RedmineUserImport
+{
+    class Program
+    {
+        private static readonly string logDir = Path.Combine(Environment.CurrentDirectory, "log");
+        private static readonly string logFile = Path.Combine(Environment.CurrentDirectory, logDir,"create.log");
+        private static readonly string userApiEndpoint = "/users.json?key=";
+        private static readonly string headerValue = "application/json";
+
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ');
+            HttpClient client = null;
+
+            try
+            {
+                Directory.CreateDirectory(logDir);
+                if (!File.Exists(logFile))
+                {
+                    File.Create(logFile).Dispose();
+                }
+                WriteLog("[INFO] " +  DateTime.Now + " Start.");
+
+                int i = 0;
+                ValidateInput(input, ref i);
+
+                client = new HttpClient();
+                if (!ValidateConnection(input[0], client))
+                {
+                    return;
+                }
+
+                using (var sr = new StreamReader(input[2], Encoding.UTF8))
+                using (var csv = new CsvHelper.CsvReader(sr))
+                {
+                    csv.Configuration.HasHeaderRecord = false;
+                    csv.Configuration.RegisterClassMap<RedmineUserImport.UserDetailMap>();
+                    var users = csv.GetRecords<UserDetail>();
+
+                    int span = i * 1000;
+                    string uri = string.Concat(input[0], userApiEndpoint, input[1]);
+                    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue(headerValue));
+                    foreach (var user in users)
+                    {
+                        string json = Newtonsoft.Json.JsonConvert.SerializeObject(new User(user));
+                        var response = client.PostAsync(uri, new StringContent(json, Encoding.UTF8, headerValue)).Result;
+                        var status = response.StatusCode.ToString();
+
+                        if (status == "Created")
+                        {
+                            WriteLog(string.Concat("Success: ", user.Mail));
+                            Console.WriteLine(string.Concat("Success: ", user.Mail));
+                        }
+                        else
+                        {
+                            WriteLog(string.Concat("Faild: ", user.Mail, " " , response.Content.ReadAsStringAsync().Result));
+                            Console.WriteLine(string.Concat(string.Concat("Faild: ", user.Mail, " ", response.Content.ReadAsStringAsync().Result)));                            
+                        }
+                        Thread.Sleep(span);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteLog(ex.Message);
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                client.Dispose();
+                WriteLog("[INFO] " + DateTime.Now + " Finish.");
+                Console.WriteLine(Environment.NewLine + "Finihs. Please enter any keys.");
+                Console.ReadLine();
+            }
+        }
+
+        static bool ValidateInput(string[] input, ref int i)
+        {
+            if (input.Length > 4)
+            {
+                Console.WriteLine("Please input three or four arguments.");
+                return false;
+            }
+
+            if (input.Length == 4)
+            {
+                if (!int.TryParse(input[3], out i))
+                {
+                    Console.WriteLine("Please input a numeric value for ther fourth argument.");
+                    return false;
+                }
+            }
+
+            if (!input[0].ToLower().StartsWith("http://") && !input[0].ToLower().StartsWith("https://"))
+            {
+                Console.WriteLine("Please add 'http://' or 'https://' on your url.");
+                return false;
+            }
+            return true;
+        }
+
+        static bool ValidateConnection(string url, HttpClient client)
+        {
+            var response = client.GetAsync(url).Result;
+            if (response.StatusCode.ToString() != "OK")
+            {
+                Console.WriteLine("Can not reach {0}", url);
+                return false;
+            }
+            return true;
+        }
+
+        static void WriteLog(string s)
+        {
+            File.AppendAllText(logFile, s + Environment.NewLine, Encoding.UTF8);
+        }
+    }
+}
---
+++ RedmineUserImport/User.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+
+namespace RedmineUserImport
+{
+    [JsonObject("user")]
+    public class User
+    {
+        [JsonProperty("user")]
+        public UserDetail _userDetail { get; set; } = new UserDetail();
+
+        public User (UserDetail deatil)
+        {
+            this._userDetail.Login = deatil.Login;
+            this._userDetail.Password = deatil.Password;
+            this._userDetail.FirstName = deatil.FirstName;
+            this._userDetail.LastName = deatil.LastName;
+            this._userDetail.Mail = deatil.Mail;
+            this._userDetail.MustChangePasswd = deatil.MustChangePasswd;
+        }
+    }
+
+    public class UserDetail
+    {
+        [JsonProperty("login")]
+        public string Login { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
+
+        [JsonProperty("firstname")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("lastname")]
+        public string LastName { get; set; }
+
+        [JsonProperty("mail")]
+        public string Mail { get; set; }
+
+        [JsonProperty("must_change_passwd")]
+        public bool MustChangePasswd { get; set; }
+    }
+
+    public sealed class UserDetailMap : CsvHelper.Configuration.CsvClassMap<UserDetail>
+    {
+        public UserDetailMap()
+        {
+            Map(x => x.Login).Index(0);
+            Map(x => x.Password).Index(1);
+            Map(x => x.FirstName).Index(2);
+            Map(x => x.LastName).Index(3);
+            Map(x => x.Mail).Index(4);
+            Map(x => x.MustChangePasswd).Index(5);
+        }
+    }
+}
